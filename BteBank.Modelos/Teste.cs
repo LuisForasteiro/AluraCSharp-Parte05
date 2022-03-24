6f55f0c24787fdb78525339567940cc65a3b4959
LuisForasteiro <luisfelipes.cg@gmail.com>
2022-03-24 13:26:20 -0400

    Add project files.

---
+++ BteBank.Modelos/Teste.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BteBank.Modelos
+{
+    class ClasseTeste
+    {
+        public ClasseTeste()
+        {
+            ModificadoresTeste teste = new ModificadoresTeste();
+            teste.MetodoPublico();    
+            teste.MetodoInterno();
+
+        }
+    }
+
+    class ClasseDerivada : ModificadoresTeste
+    {
+        public ClasseDerivada()
+            {
+                MetodoProtegido();
+            }
+            
+    }
+
+
+
+    public class ModificadoresTeste
+    {
+        public void MetodoPublico()
+        {
+            //visivel fora da classse de "Modificadores"
+        }
+
+        private void MetodoPrivado()
+        {
+            //visivel apenas na classe de "ModificadoresTeste"
+
+        }
+
+        protected void MetodoProtegido()
+        {
+            //Visivel apenas na classe "Modificadores Teste" e derivados
+        }
+
+        internal void MetodoInterno()
+        {
+            //visivel apenas para o projeto ByteBank.Modelos!
+        }
+    }
+
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(8888, 98766);
+
+            
+            Console.WriteLine(conta.Numero);
+
+
+            Console.ReadLine(); 
+            
+        }
+    }
+}
